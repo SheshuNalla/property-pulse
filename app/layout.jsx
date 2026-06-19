961b92ac968283128970a6a84fd302d3db5e9446
SheshuNalla <sheshunalla002@gmail.com>
2026-06-19 13:07:52 +0530

    feat(Homepage): add Navbar with logo and profile

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,5 @@
-import '@/assets/styles/globals.css'
+import '@/assets/styles/global.css'
+import Navbar from '@/components/Navbar'
 export const metadata = {
     title: "PropertyPulse | Find The Perfect Rental",
     description: "Find Your Dream Rental Property",
@@ -9,7 +10,8 @@ const MainLayout = ({children}) => {
   return (
     <html>
         <body>
-            <div>{children}</div>
+          <Navbar/>
+            <main>{children}</main>
         </body>
         
     </html>
