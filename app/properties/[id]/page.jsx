7bd171919e6e3fefffe946ac8cd23fc6fff4a7f0
SheshuNalla <sheshunalla002@gmail.com>
2026-06-19 11:40:43 +0530

    used next/navigation routers

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
-
+"use client"
+import { useRouter, useParams, useSearchParams } from 'next/navigation'
 const PropertyPage = () => {
+  console.log('Hello')
+  const router = useRouter();
+  const SearchParams = useSearchParams();
+  const {id} = useParams();
+  const name = SearchParams.get('name');
   return (
-    <div>PropertyPage</div>
+    <div>
+      <button onClick={() => router.push('/')} className="bg-blue-500 text-white">Go Home {name}</button>
+    </div>
   )
 }
 
