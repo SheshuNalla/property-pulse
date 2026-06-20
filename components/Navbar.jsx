ce4399a1661a9cacc16dca9288650c4b8584b0ce
SheshuNalla <sheshunalla002@gmail.com>
2026-06-20 15:27:29 +0530

    fix(mobile menu): update login status

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -232,13 +232,15 @@ const Navbar = () => {
                   >Add Property</Link
                 >
                 )}
-                
-                <button
+                {!isLoggedIn && (
+                  <button
                   className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-4"
                 >
                   <FaGoogle className='text-white mr-2'/>
                   <span>Login or Register</span>
                 </button>
+                )}
+                
               </div>
               </div>
             )}
