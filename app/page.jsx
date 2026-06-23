bc538f92f427fff90605bd945967ac09148fdd15
SheshuNalla <sheshunalla002@gmail.com>
2026-06-23 12:02:03 +0530

    feat(Homepage): add Hero section and InfoBoxes

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,11 @@
-import Link from 'next/link';
+import Hero from '@/components/Hero';
+import InfoBoxes from '@/components/InfoBoxes';
 const HomePage = () => {
   return (
-    <div>
-      <h1 className="text-5xl">Welcome</h1>
-      <Link href="./properties" className="text-4xl">Properties Page</Link>
-    </div>
+    <>
+    <Hero />
+    <InfoBoxes/>
+    </>
   )
 }
 
