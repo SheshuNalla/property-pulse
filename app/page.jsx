1c54e78690874b5846649db8e73cbd68ae25007e
SheshuNalla <sheshunalla002@gmail.com>
2026-06-26 13:01:51 +0530

    feat(routing): add first api route

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,8 @@
 import Hero from '@/components/Hero';
 import HomeProperties from '@/components/HomeProperties';
 import InfoBoxes from '@/components/InfoBoxes';
-const HomePage = async () => {
+const HomePage = () => {
+  
   return (
     <>
     <Hero />
