06aefd19089943ed1e69b4b5b29bba217050a923
SheshuNalla <sheshunalla002@gmail.com>
2026-07-01 00:35:27 +0530

    fix(route): add requests file

--- components/HomeProperties.jsx
+++ components/HomeProperties.jsx
@@ -1,19 +1,7 @@
 import Link from 'next/link';
 import PropertyCard from './PropertyCard';
+import { fetchProperties } from '@/utils/requests';
 
-async function fetchProperties(){
-  try {
-    const res =  await fetch(`${process.env.NEXT_PUBLIC_API_DOMAIN}/properties`)
-
-    if(!res.ok){
-      throw new Error('Failed to fetch properties');
-    }
-
-    return res.json();
-  } catch (error) {
-    console.log(error);
-  }
-}
 const HomeProperties =async() => {
 
     const properties = await fetchProperties();
