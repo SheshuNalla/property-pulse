7f9367d3e44c266f0e83c9b93473397656aeb029
SheshuNalla <sheshunalla002@gmail.com>
2026-07-16 12:34:47 +0530

    feat(profile): add Delete property listing

--- app/api/properties/[id]/route.js
+++ app/api/properties/[id]/route.js
@@ -1,5 +1,6 @@
 import connectDB from "@/config/database";
 import Property from "@/models/Property";
+import { getSessionUser } from "@/utils/getSessionUser";
 
 // GET api/properties:[id]
 export const GET = async (request, {params}) => {
@@ -12,6 +13,40 @@ export const GET = async (request, {params}) => {
             return new Response('Property Not Found',{status: 404})
 
         return new Response(JSON.stringify(property), {status : 200})
+    } catch (error) {
+        return new Response('Something went wrong', {status: 500})   
+    }
+}
+
+// DELETE api/properties:[id]
+export const DELETE = async (request, {params}) => {
+    try {
+        const {id} = await params;
+        
+
+        const sessionUser = await getSessionUser();
+
+        //Check for a session
+        if(!sessionUser || !sessionUser.userId){
+            return new Response('user ID is required',{status:401});
+        }
+
+        const {userId} = sessionUser;
+        await connectDB();
+        
+        const property = await Property.findById(id);
+
+        if(!property) 
+            return new Response('Property Not Found',{status: 404})
+
+        // Verify ownership
+        if(property.owner.toString() !== userId){
+            return new Response('Unauthorized',{status: 401});
+        }
+
+        await property.deleteOne();
+
+        return new Response('Property Deleted', {status : 200})
     } catch (error) {
         return new Response('Something went wrong', {status: 500})   
     }
--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -37,8 +37,27 @@ const ProfilePage = () => {
       }
     },[session])
 
-    const handleDeleteProperty = () =>{
+    const handleDeleteProperty = async (propertyId) =>{
+      console.log(propertyId)
+      const confirmed = window.confirm('Are you sure you want to delete this property?');
 
+      if(!confirmed) return;
+
+      try {
+        const res = await fetch(`/api/properties/${propertyId}`,{method: 'DELETE'});
+
+        if(res.status === 200){
+          // Remove property from state
+          const updatedProperties = properties.filter((property) => property._id !== propertyId);
+
+          setProperties(updatedProperties);
+          alert('Property Deleted')
+        }else{
+          alert('Failed to delete property');
+        }
+      } catch (error) {
+        console.log(error);
+      }
     };
   return (
     <section className="bg-blue-50">
