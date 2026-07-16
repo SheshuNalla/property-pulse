edac5bce73fe8b50d1e9e76d592534b8ef1d1c87
SheshuNalla <sheshunalla002@gmail.com>
2026-07-16 11:06:03 +0530

    feat(profile): add profile properties listings

---
+++ app/api/properties/user/[userId]/route.js
@@ -0,0 +1,21 @@
+import connectDB from "@/config/database";
+import Property from "@/models/Property";
+
+
+// GET api/properties/user/:userId
+export const GET = async (request, {params}) => { 
+    try {
+        await connectDB();
+
+        const {userId} = await params;
+
+        if(!userId){
+            return new Response('userId is required',{status: 400});
+        }
+
+        const properties = await Property.find({owner: userId});
+        return new Response(JSON.stringify(properties), {status : 200})
+    } catch (error) {
+        return new Response('Something went wrong', {status: 500})   
+    }
+}
