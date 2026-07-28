763c4e1c7d1e7626031977221fd467989aca352b
SheshuNalla <sheshunalla002@gmail.com>
2026-07-28 12:13:46 +0530

    feat(messages): add delete functionality

--- app/api/messages/[id]/route.js
+++ app/api/messages/[id]/route.js
@@ -5,7 +5,6 @@ import { getSessionUser } from "@/utils/getSessionUser";
 export const dynamic = 'force-dynamic';
 
 // PUT api/messages/[id]
-
 export const PUT = async (request, { params }) => {
     try{
         await connectDB();
@@ -37,3 +36,33 @@ export const PUT = async (request, { params }) => {
         return new Response('Something went wrong', {status:500});
     }
 };
+
+// DELETE /api/messages/[id]
+export const DELETE = async (request, {params}) => {
+    try{
+        await connectDB();
+
+        const { id } = await params;
+
+        const sessionUser = await getSessionUser();
+        if(!sessionUser || !sessionUser.user){
+            return new Response('User ID is required', {status:401});
+        }
+        const {userId} = sessionUser;
+
+        const message = await Message.findById(id);
+
+        if(!message) return new Response("Message Not Found", {status: 404});
+
+        if(message.recipient.toString() !== userId){
+            return new Response("Message Not Found", {status: 404});
+        }
+
+        await message.deleteOne();
+
+        return new Response('Message Deleted', {status: 200});
+    }catch(error){
+        console.log(error);
+        return new Response('Something went wrong', {status:500});
+    }
+}
