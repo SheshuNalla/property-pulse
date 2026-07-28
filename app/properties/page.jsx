7bcd2af47706189c1febd6bd981b1521a51d35d5
SheshuNalla <sheshunalla002@gmail.com>
2026-07-28 21:20:02 +0530

    feat(properties): update Properties page to client component

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,12 +1,7 @@
-import PropertyCard from '@/components/PropertyCard';
-import { fetchProperties } from '@/utils/requests';
 import PropertySearchForm from '@/components/PropertySearchForm';
+import Properties from '@/components/Properties';
 
 const PropertyPage = async() => {
-
-  const properties = await fetchProperties();
-  // Sort properties by date
-
   return (
       <>
           <section className="bg-blue-700">
@@ -14,19 +9,7 @@ const PropertyPage = async() => {
                   <PropertySearchForm />
               </div>
           </section>
-          <section className="px-4 py-6">
-            <div className="container-xl lg:container m-auto px-4 py-6">
-              {properties.length === 0 ? (
-                <p>No Properties Found</p>
-              ) : (
-              <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-                {properties.map((property) => (
-                  <PropertyCard key={property._id} property = {property}/>
-                ))}
-              </div>
-              )}
-            </div>
-          </section>
+          <Properties />
       </>
     )
 }
